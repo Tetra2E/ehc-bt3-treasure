4faff5bb3dd557af322dfe6eb0fbe5ca2153d670
Treasure Ejike <tre.ejike@gmail.com>
2023-05-12 05:09:49 +0100

    Gave variabes to product popup

--- product.js
+++ product.js
@@ -2,7 +2,11 @@ let product =document.querySelectorAll('.product')
 let details = document.getElementById('details-container')
 const det_CTA = document.querySelector('.det-CTA')
 const CTA = document.querySelector('.CTA')
-let expand = document.querySelector('.expand')
+const expand = document.querySelector('.expand')
+const image = expand.children[0]
+const drinkname = expand.children[1]
+const price = expand.children[3]
+const writeup = expand.nextElementSibling.children[0]
 
 function detail(num)
 {
@@ -10,19 +14,100 @@ function detail(num)
 
     if (num == 1) 
     {
-        expand.children[0].src = "./green_limeade.jpeg"
-        expand.children[0].alt = "Green Limeade".
-        expand.children[1].innerHTML = "<center>Green Limeade</center>"
-        expand.children[3].innerHTML ="<strike>N</strike>5,000"
-        expand.nextElementSibling.children[0].innerText = "Indulge in the refreshing taste of Green Limeade made from a blend of freshly squeezed lime,mint leaves, and ginger root for a zesty kick. Infused with the goondness of baoba fruit powder, it is rich in antioxidants and vitamin C,with just the right amount of honey, making it the perfect drink to quench your thirst"
+        image.src = "./green_limeade.jpeg"
+        image.alt = "Green Limeade"
+        drinkname.innerHTML ="<center>Green Limeade</center>"
+        price.innerHTML ="<strike>N</strike>5,000"
+        writeup.innerText = "Indulge in the refreshing taste of Green Limeade made from a blend of freshly squeezed lime,mint leaves, and ginger root for a zesty kick. Infused with the goondness of baoba fruit powder, it is rich in antioxidants and vitamin C,with just the right amount of honey, making it the perfect drink to quench your thirst"
     }
     else if (num ==2) {
-        expand.children[0].src = "./blue_lagoon.jpeg"
-        expand.children[0].alt = "Blue Lagoon"
-        expand.children[1].innerHTML = "<center>Blue Lagoon</center>"
-        expand.children[3].innerHTML ="<strike>N</strike>3,000"
-        expand.nextElementSibling.children[0].innerText = "Indulge in the refreshing taste of Green Limeade made from a blend of freshly squeezed lime,mint leaves, and ginger root for a zesty kick. Infused with the goondness of baoba fruit powder, it is rich in antioxidants and vitamin C,with just the right amount of honey, making it the perfect drink to quench your thirst"
+        image.src = "./blue_lagoon.jpeg"
+        image.alt = "Blue Lagoon"
+        drinkname.innerHTML = "<center>Blue Lagoon</center>"
+        price.innerHTML ="<strike>N</strike>3,000"
+        writeup.innerText = "Indulge in the refreshing taste of Green Limeade made from a blend of freshly squeezed lime,mint leaves, and ginger root for a zesty kick. Infused with the goondness of baoba fruit powder, it is rich in antioxidants and vitamin C,with just the right amount of honey, making it the perfect drink to quench your thirst"
     }
+    else if (num == 3)
+    {
+        image.src = ""
+        image.alt = ""
+        drinkname.innerHTML = ""
+        price.innerHTML =""
+        writeup.innerText = ""
+    }
+    else if (num == 4)
+    {
+        image.src = ""
+        image.alt = ""
+        drinkname.innerHTML = ""
+        price.innerHTML =""
+        writeup.innerText = ""
+    }
+    else if (num == 5)
+    {
+        image.src = ""
+        image.alt = ""
+        drinkname.innerHTML = ""
+        price.innerHTML =""
+        writeup.innerText = ""
+    }
+    else if (num == 6)
+    {
+        image.src = ""
+        image.alt = ""
+        drinkname.innerHTML = ""
+        price.innerHTML =""
+        writeup.innerText = ""
+    }
+    else if (num == 7)
+    {
+        image.src = ""
+        image.alt = ""
+        drinkname.innerHTML = ""
+        price.innerHTML =""
+        writeup.innerText = ""
+    }
+    else if (num == 8)
+    {
+        image.src = ""
+        image.alt = ""
+        drinkname.innerHTML = ""
+        price.innerHTML =""
+        writeup.innerText = ""
+    }
+    else if (num == 9)
+    {
+        image.src = ""
+        image.alt = ""
+        drinkname.innerHTML = ""
+        price.innerHTML =""
+        writeup.innerText = ""
+    }
+    else if (num == 10)
+    {
+        image.src = ""
+        image.alt = ""
+        drinkname.innerHTML = ""
+        price.innerHTML =""
+        writeup.innerText = ""
+    }
+    else if (num == 11)
+    {
+        image.src = ""
+        image.alt = ""
+        drinkname.innerHTML = ""
+        price.innerHTML =""
+        writeup.innerText = ""
+    }
+    else if (num == 12)
+    {
+        image.src = ""
+        image.alt = ""
+        drinkname.innerHTML = ""
+        price.innerHTML =""
+        writeup.innerText = ""
+    }
+
 }
 function det_cancel()
 {
